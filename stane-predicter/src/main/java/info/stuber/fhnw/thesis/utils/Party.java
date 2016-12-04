5d226bcd555cf8e5f732f9e92117e25f5e16083d
tstuber <tstuber@gmail.com>
2016-12-04 19:32:56 +0100

    Limited Party and Questions Range only to UK specific election.

--- stane-predicter/src/main/java/info/stuber/fhnw/thesis/utils/Party.java
+++ stane-predicter/src/main/java/info/stuber/fhnw/thesis/utils/Party.java
@@ -1,9 +1,11 @@
 package info.stuber.fhnw.thesis.utils;
 
 public enum Party {
-	UNKNOWN(0, "Unknown Party"), CON(1, "Conservative Party"), LAB(2, "Labour Party"), LD(3, "Liberal Democrats"), UKIP(
-			4,
-			"UK Independence Party"), GREEN(5, "Green Party"), SNP(6, "Scottish National Party"), PC(7, "Plaid Cymru");
+	UNKNOWN(0, "Unknown Party"), CON(1, "Conservative Party"), LAB(2, "Labour Party"), LD(3,
+			"Liberal Democrats"), UKIP(4, "UK Independence Party"), GREEN(5, "Green Party"),
+	// SNP(6, "Scottish National Party"),
+	// PC(7, "Plaid Cymru")
+	;
 
 	private final int id;
 	private final String name;
