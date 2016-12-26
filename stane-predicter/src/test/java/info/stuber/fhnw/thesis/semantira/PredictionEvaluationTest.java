d00babf74f1fc9f48fad1a6e66610b3a91f9544d
tstuber <tstuber@gmail.com>
2016-12-26 17:15:51 +0100

    Adjustment of test output.

--- stane-predicter/src/test/java/info/stuber/fhnw/thesis/semantira/PredictionEvaluationTest.java
+++ stane-predicter/src/test/java/info/stuber/fhnw/thesis/semantira/PredictionEvaluationTest.java
@@ -1,26 +1,45 @@
 package info.stuber.fhnw.thesis.semantira;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import info.stuber.fhnw.thesis.utils.GetConfigPropertyValues;
 import info.stuber.fhnw.thesis.utils.Party;
+import info.stuber.fhnw.thesis.utils.Props;
 import junit.framework.Assert;
 
 public class PredictionEvaluationTest {
 
 	PredictionEvaluation evaluator;
-	private Party PARTY = Party.fromInteger(4);
-	private int QUESTION_ID = 34;
-	private int WINDOW_SIZE = 2;
-	
+	String evaluationName;
+	private Party PARTY = Party.fromInteger(1);
+	private int QUESTION_ID = 14;
+
+	private int WINDOW_SIZE = Props.evalWindowSize();
+	private boolean ONLY_SENTIMENTAL = Props.evalOnlySentiment();
+	private boolean ONLY_FIRST_HIT = Props.evalFirstHit();
+	private boolean INVERSE_QUESTION_MODE = Props.evalInverseQuestion();
+	private String ANALYZER = Props.evalAnalyzer();
+
 	// To large Sample to send: Party 3, Question 23, Window Size 4
 
 	@Before
 	public void setup() {
-		evaluator = new PredictionEvaluation();
+		this.evaluator = new PredictionEvaluation();
+
+		// Create evaluation-Shortcut.
+		this.evaluationName = "WS" + WINDOW_SIZE + "_" + firstLetter(ONLY_SENTIMENTAL) + "_" + firstLetter(ONLY_FIRST_HIT)
+				+ "_" + firstLetter(INVERSE_QUESTION_MODE) + "_" + ANALYZER;
 	}
 
 	@Test
@@ -28,6 +47,8 @@ public class PredictionEvaluationTest {
 
 		List<PredictedResult> results = evaluator.evaluateSingle(PARTY, QUESTION_ID, WINDOW_SIZE);
 		printResult(results);
+		createReport();
+
 	}
 
 	@Test
@@ -36,26 +57,61 @@ public class PredictionEvaluationTest {
 		List<PredictedResult> results = evaluator.compareQuestion(QUESTION_ID, WINDOW_SIZE);
 		printResult(results);
 	}
-	
+
 	@Test
 	public void Evaluate_ByParty_Test() {
 
 		List<PredictedResult> results = evaluator.compareParty(PARTY, WINDOW_SIZE);
 		printResult(results);
 	}
-	
+
 	@Test
 	public void Evaluate_All_Test() {
 
 		List<PredictedResult> results = evaluator.compareAll(WINDOW_SIZE);
 		printResult(results);
 	}
-	
+
 	private void printResult(List<PredictedResult> results) {
-		System.out.println("Type\tParty\tQ-Id\tExp\tMin\t\tMax\t\tAvg\t\tW-Avg\t\tMedium");
-		
+		System.out.println("Type\tParty\tQ-Id\tExp\tMin\t\tMax\t\tAvg\t\tW-Avg\t\tMedium\t\tInvFactor");
+
 		for (PredictedResult res : results) {
 			System.out.println(res.toString());
+		}
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("\nSettings");
+		sb.append("\nWINDOW_SIZE: " + WINDOW_SIZE);
+		sb.append("\nONLY_SENTIMENTAL: " + ONLY_SENTIMENTAL);
+		sb.append("\nONLY_FIRST_HIT:" + ONLY_FIRST_HIT);
+		sb.append("\nINVERSE_QUESTION_MODE: " + INVERSE_QUESTION_MODE);
+		sb.append("\nANALYZER: " + ANALYZER);
+
+		// Create evaluation-Shortcut.
+		sb.append("\n\n" + this.evaluationName);
+		System.out.println(sb.toString());
+	}
+
+	private String firstLetter(boolean input) {
+		return (input == true) ? "T" : "F";
+	}
+
+	private void createReport() {
+
+		// Create target output file.
+
+		// set header.
+
+		// for each result: print out.
+		String S = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+		File file = new File("./reports/Report_" + this.evaluationName + "_" + S + ".html");
+		
+		try {
+			Files.write(Paths.get(file.toURI()), "My string to save".getBytes("utf-8"), StandardOpenOption.CREATE,
+					StandardOpenOption.TRUNCATE_EXISTING);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
