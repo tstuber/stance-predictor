85fd39b352de340154e0b6e29f8e5d747463778d
tstuber <tstuber@gmail.com>
2017-01-06 17:54:57 +0100

    Removal of project folder with typos.

--- stance-predicter-app/src/main/java/info/stuber/fhnw/thesis/lucene/LuceneConstants.java
+++
@@ -1,9 +0,0 @@
-package info.stuber.fhnw.thesis.lucene;
-
-public class LuceneConstants {
-	public static final String CONTENTS = "contents";
-	public static final String SOURCE = "source";
-	public static final String PARTY = "party";
-	public static final String QUESTION = "question";
-	public static final int MAX_SEARCH = 30;
-}
