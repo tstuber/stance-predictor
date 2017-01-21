7038e350c22554ef008857181df4baee1dffb05c
tstuber <tstuber@gmail.com>
2017-01-21 21:28:44 +0100

    Removal of obsolete test.

--- stance-predictor-app/src/test/java/info/stuber/fhnw/thesis/utils/QuestionTest.java
+++ stance-predictor-app/src/test/java/info/stuber/fhnw/thesis/utils/QuestionTest.java
@@ -6,11 +6,6 @@ import org.junit.Test;
 public class QuestionTest {
 
 	@Test
-	public void QuestionCount_Test() {
-		Assert.assertTrue(Question.getAllQuestions().size() == 29);
-	}
-	
-	@Test
 	public void inversionQuestion_Test() {
 		Assert.assertEquals(-1.0f, Question.questionInversionFlag(99), 0.02);
 		Assert.assertEquals(1.0f, Question.questionInversionFlag(50), 0.02);
